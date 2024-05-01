86fbb118bf3f0fba79bc02351ba4fadc680edf86
Marcos Leite <m.abner@live.co.uk>
2024-05-01 18:40:12 -0300

    HIstórico implementado

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -8,33 +8,49 @@ namespace CodeCalculadora
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
+
         public int somar(int v1, int v2)
         {
             int res = v1 + v2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int subtrair(int v1, int v2)
         {
             int res = v1 - v2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int multiplicar(int v1, int v2)
         {
             int res = v1 * v2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int dividir(int v1, int v2)
         {
             int res = v1 / v2;
-            return res; ;
+
+            listaHistorico.Insert(0, "Res: " + res);
+            return res;
         }
 
         public List<string> historico()
         {
-            return new List<string>();
+            listaHistorico.RemoveRange(3, listaHistorico.Count -3);
+            return listaHistorico;
         }
     }
 }
