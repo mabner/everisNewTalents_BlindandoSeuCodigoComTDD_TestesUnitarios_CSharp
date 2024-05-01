b12f55c3dadd86272acb3cc770b173df41305dcf
Marcos Leite <m.abner@live.co.uk>
2024-05-01 18:04:27 -0300

    Funções da calculadora implementadas

--- Calculadora/Calculadora.cs
+++ Calculadora/Calculadora.cs
@@ -10,22 +10,26 @@ namespace CodeCalculadora
     {
         public int somar(int v1, int v2)
         {
-            return 0;
+            int res = v1 + v2;
+            return res;
         }
 
         public int subtrair(int v1, int v2)
         {
-            return -1;
+            int res = v1 - v2;
+            return res;
         }
 
         public int multiplicar(int v1, int v2)
         {
-            return 0;
+            int res = v1 * v2;
+            return res;
         }
 
         public int dividir(int v1, int v2)
         {
-            return 0;
+            int res = v1 / v2;
+            return res; ;
         }
 
         public List<string> historico()
